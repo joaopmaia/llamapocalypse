42569ff40085edfc9c54a318e3b59eb85fb67ce9
Renato Rodrigues <renatoor6@gmail.com>
2018-10-31 18:48:33 -0200

    added map class and tile scrolling

--- main.lua
+++ main.lua
@@ -1,3 +1,18 @@
+Map = require("map")
+
+local map
+
+function love.load()
+  map = Map { tileset = { "LAB/wall/tile065.png",
+                          "LAB/wall/tile066.png",
+                          "LAB/wall/tile068.png",
+                          "LAB/wall/tile069.png" } }
+end
+
+function love.update(dt)
+  map:move(dt * 0.2, 0)
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+  map:draw()
 end
---
+++ map.lua
@@ -0,0 +1,104 @@
+
+Map = {
+  _map = {},
+  _tileset = {},
+  _dirty = false,
+  _x = 0,
+  _y = 0,
+  _xtiles = 0,
+  _ytiles = 0,
+  width = 0,
+  height = 0,
+}
+
+Map.__index = Map
+
+setmetatable (Map, {
+  __call = function (cls, ...)
+    local self = setmetatable ({}, cls)
+    self:_init (...)
+    return self
+  end })
+
+function Map:_init (...)
+  local args = ...
+  self.width = args.width or 800
+  self.height = args.height or 600
+  self.tile_width = args.tile_width or 64
+  self.tile_height = args.tile_height or 64
+  self._xtiles = math.ceil(self.width / self.tile_width) + 1
+  self._ytiles = math.ceil(self.height / self.tile_height)
+  if self.width % self.tile_width == 0 then
+    self._xtiles = self._xtiles + 1
+  end
+  if self.height % self.tile_height == 0 then
+    self._ytiles = self._ytiles + 1
+  end
+  if args.tileset then
+    for index, file in pairs(args.tileset) do
+      self._tileset[index] = love.graphics.newImage(file)
+    end
+    self:_init_map()
+  end
+end
+
+function Map:_init_map ()
+  for x = 0, self._xtiles do
+    self._map[x] = {}
+    for y = 0, self._ytiles do
+      self._map[x][y] = love.math.random(#self._tileset)
+    end
+  end
+  self._dirty = true
+end
+
+function Map:_pivot_map ()
+  for x = 0, self._xtiles do
+    for y = 0, self._ytiles do
+      if x == self._xtiles then
+        self._map[x][y] = love.math.random(#self._tileset)
+      else
+        self._map[x][y] = self._map[x + 1][y]
+      end
+    end
+  end
+end
+
+function Map:draw ()
+  if self._dirty then
+    if self._x > self.tile_width then
+      self:_pivot_map()
+      self._x = self._x % self.tile_width
+    end
+    local first_quad = love.graphics.newQuad(self._x, 0,
+                                             self.tile_width - self._x, self.tile_height,
+                                             self.tile_width, self.tile_height)
+    local last_quad = love.graphics.newQuad(0, 0,
+                                            self.tile_width - self._x, self.tile_height,
+                                            self.tile_width, self.tile_height)
+    local quad = love.graphics.newQuad(0, 0,
+                                       self.tile_width, self.tile_height,
+                                       self.tile_width, self.tile_height)
+    for x = 0, self._xtiles do
+      for y = 0, self._ytiles do
+        local tile = self._tileset[self._map[x][y]]
+        if x == 0 then
+          love.graphics.draw(tile, first_quad, 0, y * self.tile_height)
+        elseif x == self._xtiles then
+          love.graphics.draw(tile, last_quad, x * self.tile_width - self._x, y * self.tile_height)
+        else
+          love.graphics.draw(tile, quad, x * self.tile_width - self._x, y * self.tile_height)
+        end
+      end
+    end
+    self._dirty = false
+  end
+end
+
+function Map:move (dx, dy)
+  self._x = self._x + dx * self.tile_width
+  self._y = self._y + dy * self.tile_height
+  self._dirty = true
+end
+
+return Map
