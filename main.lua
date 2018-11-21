9178f50682f613855bc9edcff13da314f8a92611
joao.maia <joao.maia@globalhitss.com.br>
2018-11-21 16:51:18 -0200

    tela de gameover

--- main.lua
+++ main.lua
@@ -13,6 +13,7 @@ speed = 200
 local world, map
 llama_count = 0
 llamas = {}
+end_sprites = {}
 
 function load_llama_sprites ()
   local llama_sprites = {}
@@ -43,6 +44,7 @@ function clone_llama()
 end
 gamestate = "title"
 function love.load()
+  
   world = World { width = love.graphics.getWidth(),
                   height = love.graphics.getHeight(), 
                   ground = 430 }
@@ -53,14 +55,19 @@ function love.load()
               ground_tileset = { "LAB/wall/tile101.png" },
               ground = 350 }
 
+
   local llama = Llama { x = love.math.random(love.graphics.getWidth() * 0.7),
                         y = 200,
                         sprites = load_llama_sprites() }
+  for i = 0, 5 do
+    end_sprites[i] = love.graphics.newImage("Decoration/clone_" .. i .. ".png")
+  end
 
   llama:set_collision_cb(clone_llama)
   table.insert(llamas, llama)
   world:set_map(map)
   world:add_actor(llama)
+
 end
  con = 0
  pontos = 0
@@ -68,16 +75,24 @@ end
  timerCon = 0
  timerVent = 0
  decor = 0
+ endtick = 0
+ endcontrol = 0
+ 
 
 function love.update(dt)
-  
-  
   if gamestate == "title" then
     if love.keyboard.isDown("space") then
       gamestate = "play"
     end
   elseif gamestate == "over" then
-    world = nil
+    endtick = endtick + dt/dt
+  if endtick > 8 then
+    if endcontrol == 5 then
+      endcontrol = 0
+    end
+    endcontrol = (endcontrol + 1) % 6   
+    endtick = 0
+  end
     if love.keyboard.isDown("space") then
       gamestate = "title"
       love.event.quit( "restart" )
@@ -91,12 +106,14 @@ function love.update(dt)
     decor = math.random(1,3)
     pontos = pontos + dt/dt
     local prob_sci = love.math.random(1,1000)
-    if prob_sci < 5 then
+
+    if prob_sci > 5 then
       local sci = Scientist { x = 900,
                               y = 400,
                               sprites = "Scientist-Comp/Animations.png" }
       world:add_actor(sci)
     end
+
     if timerCon < con then 
       con = 0
       if decor == 1 then 
@@ -118,6 +135,7 @@ function love.update(dt)
         world:add_actor(cons)
       end
     end
+
     if timerVent < ven then 
       ven = 0
       local vent = Vent { x = 900,
@@ -125,21 +143,25 @@ function love.update(dt)
                              sprites = "industrial/vent/vent.png" }
       world:add_actor(vent)
     end
+
     world:update(dt)
     if love.keyboard.isDown("space") then
+
       for _, llama in pairs(llamas) do
         llama:jump()
       end
+
     end
   end
 end
 
 function love.draw()
   if gamestate == "title" then
-    telaini = love.graphics.newImage("Decoration/TELA.png")
+     telaini = love.graphics.newImage("Decoration/TELA.png")
      love.graphics.draw(telaini, 0, 0)
     love.graphics.print("Aperte espaço para começar o jogo!", 285, 290 )
   elseif gamestate == "over" then
+    love.graphics.draw(end_sprites[endcontrol], 0,0)
     love.graphics.print("Game Over! Aperte espaço para ir ao menu inicial.", 285, 290 )
   else
     world:draw()
